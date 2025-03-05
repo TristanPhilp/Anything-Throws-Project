aa2f6bfb98522847516e0a5f06ed3b254279aafd
killercanadian01 <justingentile90@gmail.com>
2025-03-04 21:08:43 -0500

    Update CatapultController.cs

--- Assets/Scripts/CatapultController.cs
+++ Assets/Scripts/CatapultController.cs
@@ -4,40 +4,42 @@ using UnityEngine;
 public class CatapultController : MonoBehaviour
 {
     [Header("Objects launch")]
-    public Rigidbody weight;
+   // public Rigidbody weight;
     public Rigidbody arm;
+    public bool isTurned = false;
     //HingeJoint hingeJointToDestroy;
-    public GameObject launchable;
+   // public GameObject launchable;
 
     [Header("Player Properties")]
     public bool isPlayerHere;
     public GameObject PlayerObject;
     
     // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
-    {
-        
-    }
 
     // Update is called once per frame
+
     void Update()
     {
         if (Input.GetKeyDown(KeyCode.Space) && isPlayerHere == true)
         {
 
-            weight.isKinematic = false;
-            arm.isKinematic = false;
+
+
+            if (isTurned == false)
+            {
+                isTurned = true;
+                arm.rotation = Quaternion.Euler(0, 0, 125);
+            }
+            else if (isTurned == true)
+            {
+                isTurned = false;
+                arm.rotation = Quaternion.Euler(0, 0, 45);
+            }
+            // weight.isKinematic = false;
+            // arm.isKinematic = false;
 
         }
 
-        if (Input.GetKeyUp(KeyCode.Space) && isPlayerHere == true)
-        {
-
-            HingeJoint hingeJointToDestroy;
-            hingeJointToDestroy = launchable.GetComponent<HingeJoint>();
-            Destroy(hingeJointToDestroy);
-        
-        }
     }
 
     private void OnTriggerEnter(Collider other)
@@ -45,6 +47,7 @@ public class CatapultController : MonoBehaviour
         if (other.CompareTag("Player"))
         {
             isPlayerHere = true;
+            Debug.Log("Im here");
         }
     }
 
@@ -53,16 +56,18 @@ public class CatapultController : MonoBehaviour
         if (other.CompareTag("Player"))
         {
             isPlayerHere = false;
-
-            weight.position = new Vector3(0, 4f, 1.75f);
-            weight.transform.Rotate(0, 0, 0);
-            arm.MovePosition(new Vector3(-3.731045f, 4.8f, 0));
-            arm.MoveRotation(new Quaternion(1, 0, 0, 1));
+            isTurned = false;
+            arm.rotation = Quaternion.Euler(0, 0, 45);
+            Debug.Log("Im  not here");
+            // weight.position = new Vector3(0, 4f, 1.75f);
+            // weight.transform.Rotate(0, 0, 0);
+           // arm.MovePosition(new Vector3(-3.731045f, 4.8f, 0));
+            //arm.rotation = Quaternion.Euler(0, 0, 0);
             //arm.position = new Vector3(0, 4.449043f, 2.107876f);
             //arm.transform.Rotate(-0.046f, 0.0f, 0.0f);
-            
-            weight.isKinematic = true;
-            arm.isKinematic = true;
+
+            //weight.isKinematic = true;
+            //arm.isKinematic = true;
         }
     }
 }
