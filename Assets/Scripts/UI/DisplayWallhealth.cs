31b445e3452e3fab5c23bf2b6b37347e2366f320
MoriyaFaith <46070717+MoriyaFaith@users.noreply.github.com>
2025-04-18 11:19:17 -0400

    better variable names for DisplayWallhealth.cs

--- Assets/Scripts/UI/DisplayWallhealth.cs
+++ Assets/Scripts/UI/DisplayWallhealth.cs
@@ -4,20 +4,20 @@ using UnityEngine.UI;
 
 public class DisplayWallhealth : MonoBehaviour
 {
-    public GameObject theWall;
+    public GameObject wallGameObject;
     public int currentHealth = 10;
     private int maxHealth = 100; 
-    private GameObject healthBar;
+    private GameObject healthBarGameObject;
 
     private void Start()
     {
-        healthBar = GameObject.Find("bar"); //make sure the object for the healthbar is actually set.
+        healthBarGameObject = GameObject.Find("bar"); //make sure the object for the healthbar is actually set.
     }
 
     // Update is called once per frame
     void Update()
     {
         float ratio = (float)currentHealth / (float)maxHealth;
-        healthBar.transform.localScale = Vector3.Lerp(healthBar.transform.localScale, new Vector3(ratio, 1, 1), 0.02f);
+        healthBarGameObject.transform.localScale = Vector3.Lerp(healthBarGameObject.transform.localScale, new Vector3(ratio, 1, 1), 0.02f);
     }
 }
