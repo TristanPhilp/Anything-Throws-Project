9e9ebbcac6210b2e740104d917f7e0abffcbf3f0
squidman <gonzaljm1@alfredstate.edu>
2025-04-18 00:27:41 -0400

    THE WALL.
    https://www.youtube.com/watch?v=VfBFSCjAeWc
    
    jokes and jives aside, here's the wall, now optimized and much more improved
    
    and get this: it explodes!!

--- Assets/Scripts/WallDamage.cs
+++ Assets/Scripts/WallDamage.cs
@@ -3,12 +3,15 @@ using UnityEngine;
 public class WallDamage : MonoBehaviour
 {
     //wall variables
-    public float wallHealth = 5;
-    public float objectBreakTime = 0.1f;
-    public float wallDestructTime = 0.5f;
+    public float wallHealth = 5; //The amount of objects needed to break the wall.
+    public float objectBreakTime = 0.1f; //Time till the object disappears after making collision.
+    public float wallDestructTime = 0.5f; //Time that it takes for the wall to POOF
 
 
-    public bool wallBroke = false;
+    public GameObject prefab; //Prefab that gets summoned... please make it only *the wall broken prefab*.. or else the wall will turn into that prefab lol
+
+    public bool wallBroke = false; //is the wall broke?
+                                   //ngl probably serves like 0 purpose but im keeping this just incase.
 
     //calls wall destructions and destroys object.
     void Update()
@@ -17,19 +20,26 @@ public class WallDamage : MonoBehaviour
         {
             if (wallHealth <= 0)
             {
-
-
-
                 Destroy(gameObject, wallDestructTime);
                 Debug.Log("GUP!");
+
+                Invoke("makeWall", (wallDestructTime - 0.01f));
+
                 wallBroke = true;
             }
         }
     }
 
-    //checks to see if object hits wall.
-    private void OnCollisionEnter(Collision other)
+    void makeWall()
     {
+        Instantiate(prefab, transform.position, transform.rotation);
+    }
+
+    //checks to see if object hits wall.
+    void OnTriggerEnter(Collider other)
+    {
+        Debug.Log("A object enterefd.");
+
         Debug.Log("Poopfartius | " + other.gameObject);
         if (other.gameObject.name != "Player")
         {
@@ -38,6 +48,6 @@ public class WallDamage : MonoBehaviour
         }
     }
 
-    
+
 
 }
