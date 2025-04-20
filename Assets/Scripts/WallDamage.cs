64be482c98db2243ae46cd7c445af920e5af32da
squidman <gonzaljm1@alfredstate.edu>
2025-04-20 15:51:46 -0400

    Wall Destruction II
    Okay did a fix and a new feature, essentially the debris now despawn after approx 8 seconds.
    
    They shrink after 2.5 seconds to make it less jarring and to also stylize the game!

--- Assets/Scripts/WallDamage.cs
+++ Assets/Scripts/WallDamage.cs
@@ -7,40 +7,35 @@ public class WallDamage : MonoBehaviour
     public float objectBreakTime = 0.1f; //Time till the object disappears after making collision.
     public float wallDestructTime = 0.5f; //Time that it takes for the wall to POOF
 
+    public float summonPrefabTime = 0.01f; //Time that it takes to summon the prefab
 
     public GameObject prefab; //Prefab that gets summoned... please make it only *the wall broken prefab*.. or else the wall will turn into that prefab lol
 
-    public bool wallBroke = false; //is the wall broke?
-                                   //ngl probably serves like 0 purpose but im keeping this just incase.
-
     //calls wall destructions and destroys object.
     void Update()
-    {
-        if (wallBroke == false)
+    { 
+        if (wallHealth <= 0)
         {
-            if (wallHealth <= 0)
-            {
-                Destroy(gameObject, wallDestructTime);
-                Debug.Log("GUP!");
+            Destroy(gameObject.GetComponent<BoxCollider>()); //Destroys the object hitbox.
 
-                Invoke("makeWall", (wallDestructTime - 0.01f));
+            Invoke("makeWall", (wallDestructTime - summonPrefabTime)); //Summons the wall on a delay.
+            Destroy(gameObject, wallDestructTime); //Destroys our wall.
+            Debug.Log("GUP!"); //GUP!!
 
-                wallBroke = true;
-            }
         }
+        
     }
 
     void makeWall()
     {
-        Instantiate(prefab, transform.position, transform.rotation);
+        Instantiate(prefab, transform.position, transform.rotation); //Summons prefab on top of the wall.
     }
 
     //checks to see if object hits wall.
     void OnTriggerEnter(Collider other)
     {
-        Debug.Log("A object enterefd.");
+        Debug.Log("A object entered the wall's hitbox.");
 
-        Debug.Log("Poopfartius | " + other.gameObject);
         if (other.gameObject.name != "Player")
         {
             Destroy(other.gameObject, objectBreakTime);
