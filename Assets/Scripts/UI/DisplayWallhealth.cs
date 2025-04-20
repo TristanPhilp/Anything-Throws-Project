a85da00fd4d114c40bbd3d39eb135ed73ac4785a
MoriyaFaith <46070717+MoriyaFaith@users.noreply.github.com>
2025-04-20 12:34:33 -0400

    Add functional wall to level 1, and finish health script.

--- Assets/Scripts/UI/DisplayWallhealth.cs
+++ Assets/Scripts/UI/DisplayWallhealth.cs
@@ -5,19 +5,24 @@ using UnityEngine.UI;
 public class DisplayWallhealth : MonoBehaviour
 {
     public GameObject wallGameObject;
-    public int currentHealth = 10;
-    private int maxHealth = 100; 
+    private int currentHealth;
+    private int maxHealth; 
     private GameObject healthBarGameObject;
+    private WallDamage sceneWallDamage;
 
     private void Start()
     {
         healthBarGameObject = GameObject.Find("bar"); //make sure the object for the healthbar is actually set.
+        sceneWallDamage = wallGameObject.GetComponent<WallDamage>(); //connects to wall damage script
+        maxHealth = (int)sceneWallDamage.wallHealth; //sets max health at the beginning of the scene. Since there isn't a max health variable for some reason, just grab the health as the object is made.
+        currentHealth = (int)sceneWallDamage.wallHealth; // do the same for current health when the scene is created.
     }
 
     // Update is called once per frame
     void Update()
     {
-        float ratio = (float)currentHealth / (float)maxHealth;
-        healthBarGameObject.transform.localScale = Vector3.Lerp(healthBarGameObject.transform.localScale, new Vector3(ratio, 1, 1), 0.02f);
+        currentHealth = (int)sceneWallDamage.wallHealth; //update current health
+        float ratio = (float)currentHealth / (float)maxHealth; //create a ratio to scale the health bar by.
+        healthBarGameObject.transform.localScale = Vector3.Lerp(healthBarGameObject.transform.localScale, new Vector3(ratio, 1, 1), 0.02f); //scale the health bar's x axis using a lerp. Lerping is done to make it look smooth.
     }
 }
