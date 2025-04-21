749387b8af66d54e6b991d2036044730c10f9748
killercanadian01 <justingentile90@gmail.com>
2025-04-20 20:25:33 -0400

    StartMenu

--- Assets/Scripts/UI/MainMenu.cs
+++ Assets/Scripts/UI/MainMenu.cs
@@ -114,13 +114,13 @@ public class MainMenu : MonoBehaviour
         KeepVariables.fullScreen = isFullScreen;
     }
 
-    public void SetFullScreen()
+    /**public void SetFullScreen()
     {
         if (KeepVariables.fullScreen == true)
             FullScreenToggle.isOn = true;
         else
             FullScreenToggle.isOn = false;
-    }
+    }**/
 
     //sets mouseSensX
     public void SetMouseSensitivityX(float senX)
