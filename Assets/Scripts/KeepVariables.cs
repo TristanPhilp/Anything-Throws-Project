9ba5b24d17665efa2fac120d96fc6105985a4405
killercanadian01 <justingentile90@gmail.com>
2025-04-23 02:56:12 -0400

    SaveMenuAndPrefabs
    Working on  save menu some scene prefabs and other methods are used.

--- Assets/Scripts/KeepVariables.cs
+++ Assets/Scripts/KeepVariables.cs
@@ -1,4 +1,7 @@
+using System.IO;
+using System.Runtime.Serialization.Formatters.Binary;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 //get variabales to transfer between classes
 public class KeepVariables : MonoBehaviour
@@ -10,4 +13,41 @@ public class KeepVariables : MonoBehaviour
     public static float volumeFloat;
 
     public static bool fullScreen;
+}
+
+public static class SaveSystem {
+
+    public static void SaveScene(Scene scene)
+    {
+        BinaryFormatter formatter = new BinaryFormatter();
+        string path = Application.persistentDataPath + "/player.fun";
+        FileStream stream = new FileStream(path, FileMode.Create);
+
+        Save data = new Save();
+
+        formatter.Serialize(stream, data);
+        stream.Close();
+    }
+
+    public static Save LoadScene () 
+    {
+        string path = Application.persistentDataPath + "/player.fun";
+        if (File.Exists(path))
+        {
+            BinaryFormatter formatter = new BinaryFormatter();
+            FileStream stream = new FileStream (path, FileMode.Open);
+
+            Save data = formatter.Deserialize(stream) as Save;
+            stream.Close();
+            return data;
+        }
+        else
+        { 
+            Debug.LogError("Save file not found in " + path);
+            return null;
+        }
+    
+    }
+
+
 }
---
+++ Assets/Scripts/UI/Save.cs
@@ -0,0 +1,52 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Save : MonoBehaviour
+{
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    public void SaveScene()
+    {
+
+        // PlayerPrefs.SetInt("SceneSaved", SceneManager.GetActiveScene().buildIndex);
+        SaveSystem.SaveScene(SceneManager.GetSceneByBuildIndex(1));
+    }
+    
+    public void LoadScene()
+    {
+        Save data = SaveSystem.LoadScene();
+
+    }
+
+    public void LoadPreviousScene()
+    {
+        SceneManager.LoadScene(0);
+    }
+
+    public void PlayerScene(Scene scene)
+    { 
+    
+    }
+
+    public void SaveNextScene()
+    {
+
+        PlayerPrefs.SetInt("SceneSaved", SceneManager.GetActiveScene().buildIndex + 1);
+
+    }
+
+    
+
+
+    public void LoadNextScene()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+    }
+
+    
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
