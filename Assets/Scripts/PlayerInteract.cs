2e1c4d3a73261f56ba64bb20020c4261b8b476d7
TristanPhilp <philpts@alfredstate.edu>
2025-04-25 10:32:18 -0400

    Player Hold Update
    Changed interaction distance to be 2 variables; things are held closer than they interacted.

--- Assets/Scripts/PlayerInteract.cs
+++ Assets/Scripts/PlayerInteract.cs
@@ -11,13 +11,14 @@ public class PlayerInteract : MonoBehaviour
     Joint guideHinge;
 
     public float interactDistance;
+    public float holdDistance;
     Interactable seenObject;
     private bool holding = false;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
         interactAction = InputSystem.actions.FindAction("Interact");
-        guidePoint.transform.localPosition = new Vector3(0, 0, interactDistance);
+        guidePoint.transform.localPosition = new Vector3(0, 0, holdDistance);
         guideHinge = guidePoint.GetComponent<Joint>();
     }
 
